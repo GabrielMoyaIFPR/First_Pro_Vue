69bdb50897c1113e043b5926439e30ced4fa5f3f
Gabriel Moya <gabrielmoya201415@gmail.com>
2021-11-18 22:22:11 -0300

    Primeiro commit

---
+++ first_project/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Conversor moedaA="USD" moedaB="BRL"/>
+    
+  </div>
+</template>
+
+<script>
+import Conversor from "./components/Conversor.vue";
+export default{
+  name: "app",
+  components: {
+    Conversor
+  }
+};
+</script>
+
